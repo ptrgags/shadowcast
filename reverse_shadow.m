fa2a2a97e54c17ae3d86ffeda77084272c699a15
Peter Gagliardi <pcg34@drexel.edu>
2018-08-07 21:53:55 -0400

    Proof of concept: HI

--- reverse_shadow.m
+++ reverse_shadow.m
@@ -3,7 +3,7 @@
 
 % Shadow vertices
 % Square on the table
-%
+%{
 shape = [
     -1, 2, 0;
     -1, 4, 0;
@@ -29,33 +29,57 @@ shadow = [
     1, 3.5, 0];
 %}
 
+% H shape
+%{
+shape = [
+    -1.25, 5.75, 0;
+    -1.25, 6.25, 0;
+    -0.25, 6.25, 0;
+    -0.25, 6.5, 0;
+    -1.25, 6.5, 0;
+    -1.25, 7, 0;
+    1.25, 7, 0;
+    1.25, 6.5, 0;
+    0.25, 6.5, 0;
+    0.25, 6.25, 0;
+    1.25, 6.25, 0;
+    1.25, 5.75, 0;
+];
+%}
+
+shape = [
+    -1.25, 7.5, 0;
+    -1.25, 8.75, 0;
+    -0.75, 8.75, 0;
+    -0.75, 8.25, 0;
+    0.75, 8.25, 0;
+    0.75, 8.75, 0;
+    1.25, 8.75, 0;
+    1.25, 7.5, 0;
+    0.75, 7.5, 0;
+    0.75, 8, 0;
+    -0.75, 8, 0;
+    -0.75, 7.5, 0;
+];
+
 % vertical art trading card facing down the Y-axis with the base
 % centered at the origin. Everything is measured in inches
-CARD_DIMS = [2.5, 3.5];
-card = Quad([0, 0, 3.5 / 2], [pi / 2, 0], CARD_DIMS);
+ATC_DIMS = [2.5, 3.5];
+INDEX_CARD_DIMS = [3, 5];
+card = Quad([0, 5, 5 / 2], [pi / 2, 0], INDEX_CARD_DIMS);
 
 % The light is a little bit behind and above the card
-light = [0, -4, 6];
+light = [0, 0, 11.25];
 
 table = Quad([0, 0, 0], [0, pi / 2], [20, 20]);
 shadow = shadowcast(light, card.vertices, table);
 
-% Calculate the direction from light to the shadow
-%light_dirs = light_directions(light, shadow);
-
 % Calculate the shape of the needed cutout
 cutout = shadowcast(light, shape, card);
-%{
-light_to_card = card.center - light;
-top = dot(light_to_card, card.normal);
-rep = repmat(card.normal, size(light_dirs, 1), 1);
-bottom = dot(light_dirs, rep, 2);
-t = top ./ bottom;
-
-preimage = light + t .* light_dirs;
-%}
 
 %%
+
+% Display the setup
 figure;
 view(3);
 axis equal;
@@ -89,7 +113,6 @@ rays(1:2:5, :) = shape';
 rays(2:2:6, :) = repmat(light', 1, N);
 % now plot the rays as line segments
 plot3(rays(1:2, :), rays(3:4, :), rays(5:6, :), 'r');
-
 
 % Card
 C = card.vertices;
