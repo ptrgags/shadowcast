70793bb9bfb6c4d22fe8ac30df508f9bf774f825
Peter Gagliardi <pcg34@drexel.edu>
2018-08-07 20:32:12 -0400

    Refactor code, project shadows

--- reverse_shadow.m
+++ reverse_shadow.m
@@ -4,11 +4,11 @@
 % Shadow vertices
 % Square on the table
 %
-shadow = [
-    1, 7, 0;
-    3, 7, 0;
-    3, 5, 0;
-    1, 5, 0];
+shape = [
+    -1, 2, 0;
+    -1, 4, 0;
+    1, 4, 0;
+    1, 2, 0];
 %}
 
 % M shape
@@ -32,38 +32,20 @@ shadow = [
 % vertical art trading card facing down the Y-axis with the base
 % centered at the origin. Everything is measured in inches
 CARD_DIMS = [2.5, 3.5];
-card = Quad([0, 0, 3.5 / 2], [pi / 4, pi / 4], CARD_DIMS);
-%{
-card = [
-    0, 2, 8;
-    0, 2, 0;
-    4, 2, 0;
-    4, 2, 8];
-%}
-%{
-card = [
-    0, 4, 0;
-    0, 4, 8;
-    4, 0, 8;
-    4, 0, 0;
-];
-%}
-%card_center = [2, 2, 3.5];
-%card_center = [2, 1, 3.5];
-%card_normal = [cos(pi / 6), sin(pi / 6), 0];
-%card_normal = [0, 1, 0];
-%card_normal = [sqrt(2), sqrt(2), 0];
+card = Quad([0, 0, 3.5 / 2], [pi / 2, 0], CARD_DIMS);
 
 % The light is a little bit behind and above the card
-light = [0, -4, 4];
+light = [0, -4, 6];
 
-% Calculate the direction from light to
-% the shadow vertices
-light_rays = shadow - light;
-ray_lengths = vecnorm(light_rays, 2, 2);
-light_dirs = light_rays ./ ray_lengths;
+table = Quad([0, 0, 0], [0, pi / 2], [20, 20]);
+shadow = shadowcast(light, card.vertices, table);
 
-% Calculate where the light rays intersect the card
+% Calculate the direction from light to the shadow
+%light_dirs = light_directions(light, shadow);
+
+% Calculate the shape of the needed cutout
+cutout = shadowcast(light, shape, card);
+%{
 light_to_card = card.center - light;
 top = dot(light_to_card, card.normal);
 rep = repmat(card.normal, size(light_dirs, 1), 1);
@@ -71,6 +53,7 @@ bottom = dot(light_dirs, rep, 2);
 t = top ./ bottom;
 
 preimage = light + t .* light_dirs;
+%}
 
 %%
 figure;
@@ -83,21 +66,24 @@ ylabel('Y');
 zlabel('Z');
 set(gca, 'Color', [0.1, 0.1, 0.1]);
 
-% Draw the setup in 3D
 % Shadow
-fill3(shadow(:, 1), shadow(:, 2), shadow(:, 3), 'w');
+fill3(shadow(:, 1), shadow(:, 2), shadow(:, 3), 'b');
+
+% Draw the setup in 3D
+% desired shape
+fill3(shape(:, 1), shape(:, 2), shape(:, 3), 'w');
 
 % Light
 plot3(light(1), light(2), light(3), 'ro');
 
 % Rays of light
-N = size(shadow, 1);
+N = size(shape, 1);
 % We're going to plot N line segments in 3D with 2 points each
 % but we need to rearrange the data for a call to plot3
 rays = zeros(2 * 3, N);
 % Put the shadow points in every other row. Note that
 % each point is now a spaced out column vector.
-rays(1:2:5, :) = shadow';
+rays(1:2:5, :) = shape';
 % interleave the light coordinates so every line is drawn
 % to the light source.
 rays(2:2:6, :) = repmat(light', 1, N);
@@ -110,4 +96,4 @@ C = card.vertices;
 fill3(C(:, 1), C(:, 2), C(:, 3), 'w');
 
 % Cutout
-fill3(preimage(:, 1), preimage(:, 2), preimage(:, 3), [0.7, 0.7, 0.7]);
+fill3(cutout(:, 1), cutout(:, 2), cutout(:, 3), [0.7, 0.7, 0.7]);
---
+++ shadowcast.m
@@ -0,0 +1,22 @@
+% trace rays from a light to the points, and mark where
+% they intersect a quad. If the quad is behind the points,
+% this is a shadow. If the quad is in front of the points, 
+% this is the preimage of the shadow.
+%
+% This really just computes a ray plane intersection.
+function image = shadowcast(light, points, plane)
+    light_dirs = light_directions(light, points);
+    
+    light_to_wall = plane.center - light;
+    
+    % Compute the t values along the light rays where they intersect
+    % the quad
+    top = dot(light_to_wall, plane.normal);
+    N = size(light_dirs, 1);
+    norms = repmat(plane.normal, N, 1);
+    bottom = dot(light_dirs, norms, 2);
+    t = top ./ bottom;
+
+    % Calculate the points in world space by traversing the rays
+    image = light + t .* light_dirs;
+end
