40662d647a530b308c2229da1a9774374a9daa6c
Peter Gagliardi <pcg34@drexel.edu>
2018-08-10 09:10:17 -0400

    Add command to write a shape to the PS file.

--- ShapeWriter.m
+++ ShapeWriter.m
@@ -1,6 +1,13 @@
 % Class to write shapes to a PostScript file.
 classdef ShapeWriter
+    properties(Constant)
+        % How many points in an inch
+        % for scaling the coordinates
+        POINTS_PER_INCH = 72;
+    end
+    
     properties
+        % The file handle for the PostScript file.
         ps_file;
     end
     
@@ -16,6 +23,34 @@ classdef ShapeWriter
             obj.ps_file = ps_file;
         end
         
+        function write_shape(obj, shape, comment)
+            % write the comment before the shape
+            fprintf(obj.ps_file, '\n%% %s\n', comment);
+            
+            % scale the points to be in points, not inches.
+            ps_coords = shape * obj.POINTS_PER_INCH;
+            
+            % loop over each point in the shape.
+            for i = 1:size(ps_coords, 1)
+                % In the postscript code, we move to the first point,
+                % and draw lines to all the other points.
+                if i == 1
+                    cmd = 'moveto';
+                else
+                    cmd = 'lineto';
+                end
+                
+                % Draw the points
+                x = ps_coords(i, 1);
+                y = ps_coords(i, 2);
+                fprintf(obj.ps_file, '%f %f %s\n', x, y, cmd);
+            end
+            
+            % Finish the shape
+            fprintf(obj.ps_file, 'closepath\nstroke\n');
+        end
+        
+        %
         function finish(obj)
             % Write the final showpage command
             fprintf(obj.ps_file, '\nshowpage\n');
